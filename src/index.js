fcf352001b2f8054801d535d4d9b22e1532d206a
SnO2WMaN <sno2wman.official@gmail.com>
2018-05-31 20:38:53 +0900

    Fix font loading

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import "./index.scss";
 import anime from "animejs";
 
 // Icon
-import iconSnO2WMaN from "./icons/sno2wman.svg";
-import iconQiita from "./icons/qiita.svg";
-import iconPixivFanbox from "./icons/pixiv_fanbox_1.svg";
 import fontawesome from "@fortawesome/fontawesome";
 import faTwitter from "@fortawesome/fontawesome-free-brands/faTwitter";
 import faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
@@ -13,26 +10,53 @@ import faDiscord from "@fortawesome/fontawesome-free-brands/faDiscord";
 import faAmazon from "@fortawesome/fontawesome-free-brands/faAmazon";
 import faRss from "@fortawesome/fontawesome-free-solid/faRss";
 
+import iconSnO2WMaN from "./icons/sno2wman.svg";
+import iconQiita from "./icons/qiita.svg";
+import iconPixivFanbox from "./icons/pixiv_fanbox_1.svg";
+
+import WebFont from "webfontloader";
+
 fontawesome.library.add(faTwitter, faGithub, faSteam, faDiscord, faAmazon);
 fontawesome.library.add(faRss);
 Array.from(document.querySelectorAll("i.icon")).forEach($icon => {
 	const classList = $icon.classList;
 	const $parentEl = $icon.parentElement;
-	if (classList.contains("qiita")) {
-		$parentEl.innerHTML = iconQiita;
-	} else if (classList.contains("pixiv-fanbox")) {
-		$parentEl.innerHTML = iconPixivFanbox;
-	} else if (classList.contains("sno2wman")) {
-		$parentEl.innerHTML = iconSnO2WMaN;
-	}
+	$parentEl.removeChild($icon);
+	Object.entries({
+		qiita: iconQiita,
+		"pixiv-fanbox": iconPixivFanbox,
+		sno2wman: iconSnO2WMaN
+	}).forEach(entry => {
+		if (classList.contains(entry[0])) {
+			$parentEl.innerHTML = entry[1] + $parentEl.innerHTML;
+		}
+	});
 });
 
-anime({
-	targets: document
-		.querySelector(".wrap > .icon-wrap > svg")
-		.querySelectorAll("path,circle"),
-	duration: 1500,
-	delay: (el, i) => i * 100,
-	strokeDashoffset: [anime.setDashoffset, 0],
-	easing: "easeInOutQuint"
+// Animetion
+const $wraps = Array.from(document.querySelectorAll(".contents > .wrap"));
+const $wrap1 = $wraps[0]; // Basic
+const $wrap2 = $wraps[1]; // Links
+const $wrap3 = $wraps[2]; // Skills
+const $wrap4 = $wraps[3]; // Donation
+
+const animate = () => {
+	anime({
+		targets: $wrap1
+			.querySelector(".icon-wrap > svg")
+			.querySelectorAll("path,circle"),
+		duration: 1500,
+		delay: (el, i) => i * 100,
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: "easeInOutQuint"
+	});
+};
+
+WebFont.load({
+	google: {
+		families: ["IBM Plex Sans:400,500", "IBM Plex Sans Condensed"]
+	},
+	active() {
+		animate();
+	}
 });

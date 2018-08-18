7440f294fde089426d7514508203523f826efac7
SnO2WMaN <sno2wman.official@gmail.com>
2018-08-18 10:49:13 +0900

    Add nav animation

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,7 +1,8 @@
 import webfontloader from 'webfontloader'
 
+const debug = true
 const $loading = document.getElementById('loading')
-const wait = 3000
+const $nav = document.getElementById('nav')
 
 const webfont = new Promise(resolve => {
   webfontloader.load({
@@ -20,63 +21,142 @@ const webfont = new Promise(resolve => {
   })
 })
 
-const loading = () =>
-  new Promise(r => {
-    Promise.all([
-      ...[...$loading.querySelectorAll('.loaders > .loader')].map($e => {
-        return new Promise(resolve =>
-          $e.addEventListener('animationiteration', () => {
+const loading = debug
+  ? () =>
+      new Promise(resolve => {
+        $loading.style.display = 'none'
+        resolve()
+      })
+  : () =>
+      new Promise(r => {
+        Promise.all([
+          ...[...$loading.querySelectorAll('.loaders > .loader')].map($e => {
+            return new Promise(resolve =>
+              $e.addEventListener('animationiteration', () => {
+                resolve()
+              })
+            )
+          }),
+        ])
+          .then(() =>
+            Promise.all([
+              new Promise(resolve => {
+                const $loadersWrap = $loading.querySelector('.loaders')
+                $loadersWrap.style.animationPlayState = 'running'
+                $loadersWrap.addEventListener('animationend', resolve)
+              }),
+              ...[
+                ...$loading
+                  .querySelector('.icon-wrap')
+                  .querySelectorAll('path,circle'),
+              ].map($e => {
+                $e.style.animationPlayState = 'running'
+                return new Promise(resolve =>
+                  $e.addEventListener('animationend', resolve)
+                )
+              }),
+            ])
+          )
+          .then(() => {
+            return Promise.all([
+              ...[...$loading.querySelectorAll('.faders > .fader')].map($e => {
+                $e.style.animationPlayState = 'running'
+                return new Promise(resolve =>
+                  $e.addEventListener('animationend', resolve)
+                )
+              }),
+            ])
+          })
+          .then(() => {
+            $loading.style.backgroundColor = 'transparent'
+            $loading.querySelector('.faders').style.visibility = 'hidden'
+            return Promise.all([
+              ...[...$loading.querySelectorAll('.rects .rect')].map($e => {
+                $e.style.visibility = 'visible'
+                $e.style.animationPlayState = 'running'
+                return new Promise(resolve =>
+                  $e.addEventListener('animationend', resolve)
+                )
+              }),
+            ])
+          })
+          .then(() => {
+            $loading.style.visibility = 'hidden'
+            r()
+          })
+      })
+
+const nav = () => {
+  return new Promise(resolve => {
+    $nav.style.animationPlayState = 'running'
+    $nav.addEventListener('animationend', resolve)
+  }).then(() => {
+    return Promise.all([
+      ...[...$nav.querySelectorAll('.title-wrap .site span')].map($span => {
+        return new Promise(resolve => {
+          $span.style.animationPlayState = 'running'
+          const ch = $span.innerHTML
+          const shuffler = setInterval(() => {
+            $span.innerHTML = String.fromCharCode(
+              48 + Math.floor(Math.random() * 74)
+            )
+          }, 40)
+          $span.addEventListener('animationend', () => {
+            clearInterval(shuffler)
+            $span.innerHTML = ch
             resolve()
           })
-        )
+        })
       }),
-    ])
-      .then(() =>
-        Promise.all([
-          new Promise(resolve => {
-            const $loadersWrap = $loading.querySelector('.loaders')
-            $loadersWrap.style.animationPlayState = 'running'
-            $loadersWrap.addEventListener('animationend', resolve)
-          }),
-          ...[
-            ...$loading
-              .querySelector('.icon-wrap')
-              .querySelectorAll('path,circle'),
-          ].map($e => {
-            $e.style.animationPlayState = 'running'
-            return new Promise(resolve =>
-              $e.addEventListener('animationend', resolve)
-            )
-          }),
-        ])
-      )
-      .then(() =>
-        Promise.all([
-          ...[...$loading.querySelectorAll('.faders > .fader')].map($e => {
-            $e.style.animationPlayState = 'running'
-            return new Promise(resolve =>
-              $e.addEventListener('animationend', resolve)
-            )
-          }),
-        ])
-      )
-      .then(() => {
-        $loading.querySelector('.faders').style.visibility = 'hidden'
-        $loading.style.backgroundColor = null
+      ...[...$nav.querySelectorAll('.title-wrap .profile span')].map($span => {
+        return new Promise(resolve => {
+          $span.style.animationPlayState = 'running'
+          $span.addEventListener('animationend', resolve)
+        })
+      }),
+      ...[...$nav.querySelectorAll('.socials > .social')].map($social => {
+        $social.classList.add('running')
         return Promise.all([
-          ...[...$loading.querySelectorAll('.rects .rect')].map($e => {
-            $e.style.visibility = 'visible'
-            $e.style.animationPlayState = 'running'
-            return new Promise(resolve =>
-              $e.addEventListener('animationend', resolve)
-            )
-          }),
+          ...[
+            ...$social.querySelectorAll(
+              '.covers > .cover,.icon,.borders > .border'
+            ),
+          ].map(
+            $e =>
+              new Promise(resolve =>
+                $e.addEventListener('animationend', resolve)
+              )
+          ),
         ])
-      })
-      .then(() => {
-        $loading.style.visibility = 'hidden'
-        r()
-      })
+      }),
+      ...[...$nav.querySelectorAll('.menu > li')].map($social => {
+        $social.classList.add('running')
+        return Promise.all([
+          ...[...$social.querySelectorAll('p,.covers > .cover,.border')].map(
+            $e =>
+              new Promise(resolve =>
+                $e.addEventListener('animationend', resolve)
+              )
+          ),
+        ])
+      }),
+      ...[...$nav.querySelectorAll('.licence-wrap > .licence')].map(
+        $licence => {
+          $licence.classList.add('running')
+          return Promise.all([
+            ...[...$licence.querySelectorAll('p,border')].map(
+              $e =>
+                new Promise(resolve =>
+                  $e.addEventListener('animationend', resolve)
+                )
+            ),
+          ])
+        }
+      ),
+    ])
   })
+}
 
-export default Promise.all([webfont]).then(() => loading())
+export default Promise.all([webfont])
+  .then(() => loading())
+  .then(() => nav())

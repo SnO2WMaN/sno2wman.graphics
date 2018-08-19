4b0f966487032d0e68e4e04fc97fd1d4bb7daf8e
SnO2WMaN <sno2wman.official@gmail.com>
2018-08-19 13:39:44 +0900

    Add option for imagemin (parcel-plugin-imagemin#14)

---
+++ imagemin.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mozjpeg: {
+    progressive: true,
+    quality: 65,
+  },
+  pngquant: {
+    quality: '65-90',
+    speed: 4,
+  },
+  gifsicle: {
+    interlaced: false,
+    optimizationLevel: 3,
+  },
+}

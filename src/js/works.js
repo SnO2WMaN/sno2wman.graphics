039264dadfdb10573a07e3f360894336000edc20
SnO2WMaN <sno2wman.official@gmail.com>
2018-08-28 19:07:04 +0900

    Works filtering

--- src/js/works.js
+++ src/js/works.js
@@ -8,14 +8,15 @@ export default Barba.BaseView.extend({
     nav.moved('works')
   },
   onEnterCompleted() {
-    const $menu = document.getElementById('menu')
-    $menu.classList.add('animate')
+    const $worksWrap = document.getElementById('works-wrap')
+    const $menuWrap = document.getElementById('menu')
 
-    const $worksRoot = document.getElementById('works')
-    $worksRoot.querySelectorAll('._2018,._2017').forEach($year => {
+    $menuWrap.classList.add('animate')
+
+    $worksWrap.querySelectorAll('._2018,._2017').forEach($year => {
       const $works = $year.querySelector('.works')
       $works.querySelectorAll('.work').forEach(($work, i) => {
-        $work.style.animationDelay = `${i * 0.15}s`
+        $work.style.animationDelay = `${(i - 1) * 0.15}s`
       })
       const msnry = new Masonry($works, {
         itemSelector: '.work',
@@ -23,11 +24,46 @@ export default Barba.BaseView.extend({
         percentPosition: true,
         transitionDuration: 0,
       })
+      const $elements = msnry.getItemElements()
       msnry.on('layoutComplete', () => {
         $year.querySelector('.title-wrap').classList.add('loaded')
         $works.classList.add('loaded')
+        if (!msnry.filter) {
+          m(msnry, $year, $elements)
+        }
+        msnry.filter = true
       })
       msnry.layout()
     })
+
+    function m(msnry, $year, $elements) {
+      $menuWrap.querySelectorAll('.genre,.type').forEach($menus => {
+        $menus.querySelectorAll(':scope li').forEach($menu => {
+          const selector = $menu.getAttribute('data-selector')
+          $menu.addEventListener('click', () => {
+            $worksWrap.scrollTop = 0
+            if ($menus.querySelector(':scope .selected')) {
+              $menus
+                .querySelector(':scope .selected')
+                .classList.remove('selected')
+            }
+            $menu.classList.add('selected')
+
+            const $yearWorks = $year.querySelector('.works')
+            const appended = $elements.filter($work =>
+              $work
+                .getAttribute('data-tags')
+                .split(',')
+                .includes(selector)
+            )
+            msnry.remove(msnry.getItemElements())
+            $yearWorks.append(...appended)
+            msnry.appended(appended)
+            msnry.reloadItems()
+            msnry.layout()
+          })
+        })
+      })
+    }
   },
 })

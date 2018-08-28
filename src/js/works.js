9a98a358ec41387a79b6c77075b41ccd635f443b
SnO2WMaN <sno2wman.official@gmail.com>
2018-08-29 02:49:46 +0900

    Works nav update

--- src/js/works.js
+++ src/js/works.js
@@ -13,60 +13,77 @@ export default Barba.BaseView.extend({
 
     $menuWrap.classList.add('animate')
 
-    $worksWrap.querySelectorAll('._2018,._2017').forEach($year => {
-      const $works = $year.querySelector('.works')
-      $works.querySelectorAll('.work').forEach(($work, i) => {
-        $work.style.animationDelay = `${i * 0.15}s`
+    const $years = [...$worksWrap.querySelectorAll('._2018,._2017')]
+    const rayoutInits = $years.map(
+      $year =>
+        new Promise(resolve => {
+          const $works = $year.querySelector('.works')
+          $works.querySelectorAll('.work').forEach(($work, i) => {
+            $work.style.animationDelay = `${i * 0.15}s`
+          })
+          const msnry = new Masonry($works, {
+            itemSelector: '.work',
+            columnWidth: '.sizer',
+            percentPosition: true,
+            transitionDuration: 0,
+          })
+          msnry.once('layoutComplete', () => {
+            $year.msnry = msnry
+            $year.$all = msnry.getItemElements()
+            $year.querySelector('.title-wrap').classList.add('loaded')
+            $works.classList.add('loaded')
+            resolve()
+          })
+          msnry.layout()
+        })
+    )
+    const inited = () => {
+      $menuWrap.querySelectorAll('.year > li').forEach($menu => {
+        const selector = $menu.getAttribute('data-selector')
+        $menu.addEventListener('click', () => {
+          $worksWrap.scrollTop = $worksWrap.querySelector(
+            `._${selector}`
+          ).offsetTop
+        })
       })
-      const msnry = new Masonry($works, {
-        itemSelector: '.work',
-        columnWidth: '.sizer',
-        percentPosition: true,
-        transitionDuration: 0,
-      })
-      const $elements = msnry.getItemElements()
-      msnry.on('layoutComplete', () => {
-        $year.querySelector('.title-wrap').classList.add('loaded')
-        $works.classList.add('loaded')
-        if (!msnry.filter) {
-          m(msnry, $year, $elements)
-        }
-        msnry.filter = true
-      })
-      msnry.layout()
-    })
-
-    function m(msnry, $year, $elements) {
-      $menuWrap.querySelectorAll('.genre,.type').forEach($menus => {
-        $menus.querySelectorAll(':scope li').forEach($menu => {
-          const selector = $menu.getAttribute('data-selector')
-          $menu.addEventListener('click', () => {
-            $worksWrap.scrollTop = 0
-            if ($menus.querySelector(':scope .selected')) {
-              $menus
-                .querySelector(':scope .selected')
-                .classList.remove('selected')
+      $menuWrap.querySelectorAll('.genre > li,.type > li').forEach($menu => {
+        const selector = $menu.getAttribute('data-selector')
+        $menu.addEventListener('click', () => {
+          $worksWrap.scrollTop = 0
+          const selected = $menu.classList.contains('selected')
+          if (selected) {
+            $menu.classList.remove('selected')
+          } else {
+            const $before = $menuWrap.querySelector('.selected')
+            if ($before) {
+              $before.classList.remove('selected')
             }
             $menu.classList.add('selected')
-
-            const $yearWorks = $year.querySelector('.works')
-            const appended = $elements.filter($work =>
-              $work
-                .getAttribute('data-tags')
-                .split(',')
-                .includes(selector)
-            )
-            appended.forEach(($work, i) => {
+          }
+          $years.forEach($year => {
+            const $works = $year.querySelector('.works')
+            const msnry = $year.msnry
+            const $all = $year.$all
+            const $appended = selected
+              ? $all
+              : $all.filter($work =>
+                  $work
+                    .getAttribute('data-tags')
+                    .split(',')
+                    .includes(selector)
+                )
+            $appended.forEach(($work, i) => {
               $work.style.animationDelay = `${i * 0.15}s`
             })
             msnry.remove(msnry.getItemElements())
-            $yearWorks.append(...appended)
-            msnry.appended(appended)
+            $works.append(...$appended)
+            msnry.appended($appended)
             msnry.reloadItems()
             msnry.layout()
           })
         })
       })
     }
+    Promise.all(rayoutInits).then(inited)
   },
 })

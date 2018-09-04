c28ba34d826edb67cecfc4ad687d911c75f770c7
SnO2WMaN <sno2wman.official@gmail.com>
2018-09-04 10:24:09 +0900

    Font Awesome Tree shaking

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,4 +1,7 @@
+/* eslint-disable import/max-dependencies */
+
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
+/*
 import {
   faEnvelope,
   faCopy,
@@ -23,7 +26,29 @@ import {
   faYoutube,
   faSoundcloud,
   faMastodon,
-} from '@fortawesome/free-brands-svg-icons'
+} from '@fortawesome/free-brands-svg-icons' */
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt'
+import { faMars } from '@fortawesome/free-solid-svg-icons/faMars'
+import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons/faBirthdayCake'
+import { faClock } from '@fortawesome/free-solid-svg-icons/faClock'
+import { faUmbrellaBeach } from '@fortawesome/free-solid-svg-icons/faUmbrellaBeach'
+import { faPastafarianism } from '@fortawesome/free-solid-svg-icons/faPastafarianism'
+
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
+import { faTumblr } from '@fortawesome/free-brands-svg-icons/faTumblr'
+import { faTwitch } from '@fortawesome/free-brands-svg-icons/faTwitch'
+import { faSpotify } from '@fortawesome/free-brands-svg-icons/faSpotify'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord'
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub'
+import { faSteam } from '@fortawesome/free-brands-svg-icons/faSteam'
+import { faCodepen } from '@fortawesome/free-brands-svg-icons/faCodepen'
+import { faDribbble } from '@fortawesome/free-brands-svg-icons/faDribbble'
+import { faBehance } from '@fortawesome/free-brands-svg-icons/faBehance'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube'
+import { faSoundcloud } from '@fortawesome/free-brands-svg-icons/faSoundcloud'
+import { faMastodon } from '@fortawesome/free-brands-svg-icons/faMastodon'
 
 library.add(
   // solid

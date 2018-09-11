d1b3e3d10e700fea4c9b47115f5e295db56ffd05
SnO2WMaN <sno2wman.official@gmail.com>
2018-09-11 11:09:40 +0900

    Add skillset

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,32 +1,7 @@
 /* eslint-disable import/max-dependencies */
 
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
-/*
-import {
-  faEnvelope,
-  faCopy,
-  faExternalLinkAlt,
-  faMars,
-  faBirthdayCake,
-  faClock,
-  faUmbrellaBeach,
-  faPastafarianism,
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faTwitter,
-  faTumblr,
-  faTwitch,
-  faSpotify,
-  faDiscord,
-  faGithub,
-  faSteam,
-  faCodepen,
-  faDribbble,
-  faBehance,
-  faYoutube,
-  faSoundcloud,
-  faMastodon,
-} from '@fortawesome/free-brands-svg-icons' */
+
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope'
 import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt'
@@ -35,6 +10,14 @@ import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons/faBirthdayCake
 import { faClock } from '@fortawesome/free-solid-svg-icons/faClock'
 import { faUmbrellaBeach } from '@fortawesome/free-solid-svg-icons/faUmbrellaBeach'
 import { faPastafarianism } from '@fortawesome/free-solid-svg-icons/faPastafarianism'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons/faCodeBranch'
+import { faBuilding } from '@fortawesome/free-solid-svg-icons/faBuilding'
+import { faCogs } from '@fortawesome/free-solid-svg-icons/faCogs'
+import { faBroom } from '@fortawesome/free-solid-svg-icons/faBroom'
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode'
+import { faToolbox } from '@fortawesome/free-solid-svg-icons/faToolbox'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt'
+import { faRocket } from '@fortawesome/free-solid-svg-icons/faRocket'
 
 import { faAmazon } from '@fortawesome/free-brands-svg-icons/faAmazon'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter'
@@ -51,6 +34,9 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons/faYoutube'
 import { faSoundcloud } from '@fortawesome/free-brands-svg-icons/faSoundcloud'
 import { faMastodon } from '@fortawesome/free-brands-svg-icons/faMastodon'
 import { faPaypal } from '@fortawesome/free-brands-svg-icons/faPaypal'
+import { faHtml5 } from '@fortawesome/free-brands-svg-icons/faHtml5'
+import { faCss3 } from '@fortawesome/free-brands-svg-icons/faCss3'
+import { faJs } from '@fortawesome/free-brands-svg-icons/faJs'
 
 library.add(
   // solid
@@ -62,6 +48,14 @@ library.add(
   faClock,
   faUmbrellaBeach,
   faPastafarianism,
+  faCodeBranch,
+  faBuilding,
+  faCogs,
+  faBroom,
+  faCode,
+  faPencilAlt,
+  faToolbox,
+  faRocket,
   // bland
   faAmazon,
   faTwitter,
@@ -77,6 +71,9 @@ library.add(
   faSoundcloud,
   faMastodon,
   faCodepen,
-  faPaypal
+  faPaypal,
+  faHtml5,
+  faCss3,
+  faJs
 )
 dom.watch()

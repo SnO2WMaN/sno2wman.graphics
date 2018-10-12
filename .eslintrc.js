fe5baf366d541b8982ad8ae0d0409f566a370d01
SnO2WMaN <sno2wman.official@gmail.com>
2018-10-13 04:17:22 +0900

    Use eslint-config-precure for linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,4 @@
-const prettier = require('./.prettierrc.js')
-
 module.exports = {
     root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    parserOptions: {
-        parser: 'babel-eslint',
-        sourceType: 'module',
-        ecmaVersion: 9,
-    },
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-    rules: {
-        'prettier/prettier': ['error', prettier],
-    },
+    extends: 'precure',
 }

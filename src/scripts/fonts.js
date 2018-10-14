51a560d81207e9d25543e5b83c10ace77c2e4970
SnO2WMaN <sno2wman.official@gmail.com>
2018-10-14 18:01:15 +0900

    Description

--- src/scripts/fonts.js
+++ src/scripts/fonts.js
@@ -3,7 +3,7 @@ import WebfontLoader from 'webfontloader'
 export default new Promise(resolve => {
 	WebfontLoader.load({
 		google: {
-			families: ['Cutive Mono', 'Dosis', 'Assistant', 'Noto Sans JP'],
+			families: ['Cutive Mono', 'Dosis', 'Assistant', 'Sawarabi Gothic'],
 		},
 		active: resolve,
 	})

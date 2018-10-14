3f3576564e1a4ab72d0b00c3907454761406f6e8
SnO2WMaN <sno2wman.official@gmail.com>
2018-10-14 09:34:32 +0900

    Optimize FontAwesome Treeshaking

--- src/scripts/fontawesome.js
+++ src/scripts/fontawesome.js
@@ -1,4 +1,6 @@
+/* eslint-disable import/max-dependencies */
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
+/*
 import {
 	faTwitter,
 	faGithub,
@@ -7,7 +9,6 @@ import {
 	faCodepen,
 	faSteam,
 	faTumblr,
-	faMastodon,
 	faTwitch,
 } from '@fortawesome/free-brands-svg-icons'
 
@@ -22,6 +23,26 @@ import {
 	faExternalLinkAlt,
 	faClipboard,
 } from '@fortawesome/free-solid-svg-icons'
+*/
+
+import faTwitter from '@fortawesome/free-brands-svg-icons/faTwitter'
+import faGithub from '@fortawesome/free-brands-svg-icons/faGithub'
+import faDiscord from '@fortawesome/free-brands-svg-icons/faDiscord'
+import faSpotify from '@fortawesome/free-brands-svg-icons/faSpotify'
+import faCodepen from '@fortawesome/free-brands-svg-icons/faCodepen'
+import faTumblr from '@fortawesome/free-brands-svg-icons/faTumblr'
+import faSteam from '@fortawesome/free-brands-svg-icons/faSteam'
+import faTwitch from '@fortawesome/free-brands-svg-icons/faTwitch'
+
+import faEnvelope from '@fortawesome/free-solid-svg-icons/faEnvelope'
+import faMars from '@fortawesome/free-solid-svg-icons/faMars'
+import faBirthdayCake from '@fortawesome/free-solid-svg-icons/faBirthdayCake'
+import faUmbrellaBeach from '@fortawesome/free-solid-svg-icons/faUmbrellaBeach'
+import faPastafarianism from '@fortawesome/free-solid-svg-icons/faPastafarianism'
+import faClock from '@fortawesome/free-solid-svg-icons/faClock'
+import faFistRaised from '@fortawesome/free-solid-svg-icons/faFistRaised'
+import faExternalLinkAlt from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt'
+import faClipboard from '@fortawesome/free-solid-svg-icons/faClipboard'
 
 library.add(
 	// BRAND
@@ -32,7 +53,6 @@ library.add(
 	faCodepen,
 	faSteam,
 	faTumblr,
-	faMastodon,
 	faTwitch,
 	// SOLID
 	faEnvelope,

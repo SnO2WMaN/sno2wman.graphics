361533774b22cee7e3682a5c184722b8d811e5b5
SnO2WMaN <sno2wman.official@gmail.com>
2018-11-21 06:12:02 +0900

    Update header background

--- exports/header.js
+++ exports/header.js
@@ -1,11 +1,8 @@
 import SimplexNoise from 'simplex-noise'
 let noise
-
+const waveMax = 600 * 20
 let wave = 0,
 	firstHue
-const waveMax = 2000
-const spacing = 20
-
 export default {
 	/**
 	 *
@@ -13,10 +10,10 @@ export default {
 	 */
 	init(ctx) {
 		noise = new SimplexNoise(Math.random())
-		ctx.lineWidth = 1
+		ctx.lineWidth = 0.1
 		wave = 0
 		firstHue = Math.random() * 360
-		ctx.globalCompositeOperation = 'lighten'
+		ctx.globalCompositeOperation = 'xor'
 	},
 	/**
 	 *
@@ -26,18 +23,28 @@ export default {
 		if (waveMax < wave) return
 		const width = ctx.canvas.width,
 			height = ctx.canvas.height
-		let x = -(wave / waveMax) * spacing
-		ctx.strokeStyle = `hsla(${firstHue +
-			(3600 * wave) / waveMax}deg,70%,70%,.25)`
-		ctx.beginPath()
-		ctx.moveTo(0, height / 2)
-		while (x <= width + 45) {
-			const ny = Math.pow(noise.noise2D((x / width) * 12, wave / 1200), 5)
-			const fc = Math.pow(1 - Math.abs(x - width / 2) / (width / 2), 5)
-			ctx.lineTo(x, height / 2 - ny * fc * height * 1.5)
-			x += spacing
+		const spacing = width / 80
+		for (let i = 0; i < 1 + 10 * (1 - wave / waveMax); i++) {
+			let x = -(wave * 0.025)
+			const hue = firstHue + wave * 0.025
+			const lig = Math.abs(Math.cos(wave * 0.01)) * 10 + 60
+			ctx.strokeStyle = `hsla(${hue}deg,100%,${lig}%,.5)`
+			ctx.beginPath()
+			ctx.moveTo(0, height / 2)
+			while (x <= width + 45) {
+				const ny = Math.pow(
+					noise.noise2D((x / width) * 4, wave / 5000),
+					1
+				)
+				const fc = Math.pow(
+					1 - Math.abs(x - width / 2) / (width / 2),
+					3
+				)
+				ctx.lineTo(x, height / 2 - ny * fc * height * 0.25)
+				x += spacing
+			}
+			ctx.stroke()
+			wave++
 		}
-		ctx.stroke()
-		wave++
 	},
 }
